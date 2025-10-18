ddc37c68e4cce48c65ab0ed4c1d4059d4e561d90
Nikhil Tambe <134857855+nix0gg@users.noreply.github.com>
2025-10-18 08:25:38 +0530

    Add conversion factors for length, speed, mass, density, force, time, pressure, energy, power, and thermal conductivity; remove unused conversions.py file.

---
+++ maths/conversion_factors.py
@@ -0,0 +1,57 @@
+#Length
+one_meter = 0
+one_lire = 0
+one_milesq = 0
+one_gallon = 0
+one_feetcube = 0
+one_km = 0.6215 #mi
+one_mile = 1.60934 #km
+one_inch = 2.54 #cm
+one_yd = 0
+one_ly = 9.461e15 #m
+one_angstrom = 0.1e-9 #m
+
+#Area
+one_kms = 0
+
+#Volume
+
+
+#Speed
+one_kmph = 0
+one_mph = 0
+
+#Magnetic Field
+one_g = 1e-4 #T
+one_t = 0 
+
+#Mass
+one_tonne = 0
+
+#Density 
+one_gcm = 0
+
+#Angle and angular speed
+#Force
+one_newton =0
+
+#Time 
+one_hour = 0
+one_day = 0
+one_year = 0
+
+#Pressure
+one_atm = 0
+one_torr = 0
+
+#Energy
+one_ftlbf = 0
+one_btu = 0
+
+#Power
+one_hp = 0
+one_watt = 0
+
+#Thermal conductivity
+
+
--- maths/conversions.py
+++
@@ -1,5 +0,0 @@
-import math
-
-km_to_mi = 0.6215
-mi_to_km = 1.60934
-mi1 = 1609
